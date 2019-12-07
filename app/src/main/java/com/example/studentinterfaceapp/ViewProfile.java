1cba25aa9dd907b2e92b386bcccb318a8c398854
Simranbal04 <simranbal01@gmail.com>
2019-12-07 01:50:35 -0500

    ViewProfile file

--- app/src/main/java/com/example/studentinterfaceapp/ViewProfile.java
+++ app/src/main/java/com/example/studentinterfaceapp/ViewProfile.java
@@ -1,4 +1,29 @@
 package com.example.studentinterfaceapp;
 
-public class ViewProfile {
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ViewProfile extends AppCompatActivity {
+
+    TextView student_name;
+    TextView student_program;
+    TextView student_class;
+    TextView student_id;
+    Button ok_button;
+
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.viewprofile);
+
+        student_name = (TextView) findViewById(R.id.student_name);
+        student_program = (TextView) findViewById(R.id.student_program);
+        student_class = (TextView) findViewById(R.id.student_class);
+        student_id = (TextView) findViewById(R.id.student_id);
+        ok_button = (Button) findViewById(R.id.ok_button);
+
+    }
 }
