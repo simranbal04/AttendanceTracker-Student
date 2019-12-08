d91e2c2179e5bbf3ca891277d3b4fa0104f78363
Simranbal04 <simranbal01@gmail.com>
2019-12-07 23:23:54 -0500

    Student file

---
+++ app/src/main/java/com/example/studentinterfaceapp/Students.java
@@ -0,0 +1,78 @@
+package com.example.studentinterfaceapp;
+
+public class Students {
+
+    private  int studentid;
+    private  String name,program,classname,instructor,username, password;
+
+    public Students()
+    {
+
+    }
+
+    public Students(int studentid, String name, String program, String classname, String instructor, String username, String password) {
+        this.studentid = studentid;
+        this.name = name;
+        this.program = program;
+        this.classname = classname;
+        this.instructor = instructor;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getStudentid() {
+        return studentid;
+    }
+
+    public void setStudentid(int studentid) {
+        this.studentid = studentid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProgram() {
+        return program;
+    }
+
+    public void setProgram(String program) {
+        this.program = program;
+    }
+
+    public String getClassname() {
+        return classname;
+    }
+
+    public void setClassname(String classname) {
+        this.classname = classname;
+    }
+
+    public String getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(String instructor) {
+        this.instructor = instructor;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
