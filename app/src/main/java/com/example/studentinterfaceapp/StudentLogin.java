d81d3bbe89f3e0e6ca9299951104c0c403542be4
Ankita <ankitrajkaur@gmail.com>
2019-12-07 22:06:47 -0500

    Student Login New file

--- app/src/main/java/com/example/studentinterfaceapp/StudentLogin.java
+++ app/src/main/java/com/example/studentinterfaceapp/StudentLogin.java
@@ -1,7 +1,28 @@
 package com.example.studentinterfaceapp;
 
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class StudentLogin extends AppCompatActivity {
 
+
+    EditText username_et, password_et;
+
+@Override
+    protected void onCreate(Bundle savedInstanceState)
+{
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.studentlogin);
+
+    username_et = (EditText) findViewById(R.id.username_et);
+    password_et = (EditText) findViewById(R.id.password_et);
+
 }
+
+
+
+}
+
