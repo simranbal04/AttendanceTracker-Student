21c25f31bfc1a525369fc9b50b3e10b4ae00832a
Ankita <ankitrajkaur@gmail.com>
2019-12-07 23:38:56 -0500

    refactor StudentLogin

--- app/src/main/java/com/example/studentinterfaceapp/StudentLogin.java
+++ app/src/main/java/com/example/studentinterfaceapp/StudentLogin.java
@@ -1,28 +1,81 @@
 package com.example.studentinterfaceapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class StudentLogin extends AppCompatActivity {
-
+    //Database Refrences to firebase
+    FirebaseDatabase database;
+    DatabaseReference studentRefrences;
 
     EditText username_et, password_et;
 
-@Override
-    protected void onCreate(Bundle savedInstanceState)
-{
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.studentlogin);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.studentlogin);
 
-    username_et = (EditText) findViewById(R.id.username_et);
-    password_et = (EditText) findViewById(R.id.password_et);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        studentRefrences = database.getReference("students");
+        //refer to edittext from xml file
+        username_et = (EditText) findViewById(R.id.username_et);
+        password_et = (EditText) findViewById(R.id.password_et);
 
-}
+    }
 
 
+    public void studentLogin(View view) {
+//when user will click on LOGIN button, get value of username & password and store it in a varible
+        final String username = username_et.getText().toString().trim();
+        final String password = password_et.getText().toString().trim();
+        //if either username or password is empty a toast msg will show up
+        if(username.isEmpty()||password.isEmpty()){
+            Toast.makeText(getApplicationContext(),"Please Fill all Fields",Toast.LENGTH_LONG).show();
+        }
+        else {
+// make connection with database to fetch the value of username & password
+            studentRefrences.child(username).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    //if username is correct,data will exist
+                    if (dataSnapshot.exists())
+                    {
+                        Students student_data = dataSnapshot.getValue(Students.class);
+                        if (student_data.getPassword().equalsIgnoreCase(password))
+                        {
+                            Intent in = new Intent(getApplicationContext(), StudentDashboard.class);
+                            in.putExtra("username", username);
+                            startActivity(in);
+                            finish();
+                        }
+                        else{
+                            Toast.makeText(getApplicationContext(), "Wrong Password", Toast.LENGTH_LONG).show();
+                            }
+                    }
+                    else
+                        {
+                          Toast.makeText(getApplicationContext(),"Wrong username",Toast.LENGTH_LONG).show();
+                        }
+                }
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
 
+                }
+            });
+        }
+    }
 }
 
